e5eb4987b8a041c56abc81a305f6cf1e7552f416
jimee222 <jarayare@ucenfotec.ac.cr>
2025-10-26 20:05:03 -0600

    Update ProductoRepository

--- src/main/java/com/project/demo/logic/entity/producto/ProductoRepository.java
+++ src/main/java/com/project/demo/logic/entity/producto/ProductoRepository.java
@@ -1,7 +1,10 @@
 package com.project.demo.logic.entity.producto;
 
 import com.project.demo.logic.entity.producto.Producto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    Page<Producto> findByCategoryId(Long categoryId, Pageable pageable);
 }
